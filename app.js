1d602f2e88273adc05f25b9d288ed13fef961536
KeVon <kevtice15@gmail.com>
2013-02-22 03:16:55 -0500

    Pulling foursquare data to server, not yet client

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ app.get("/static/:filename", function(request, response){
 });
 
 var requestQuery;
-var responseJSON;
+var responseJSON = '';
+
+var clientId = "VU0DICU13IR5L5YWZ23OGBCIMSUA2CCQILVXMV2QRQGRGKHN";
+var clientSecret = "3UB2V5MNWB0QUCGNA5DDIH05YU0BSOOE0DI05GISLLWWGN0D";
 /*
 var options = {
 	host: 'search.twitter.com',
@@ -52,7 +55,7 @@ function tweetGetter(){
 	console.log("current query" + requestQuery);
 	var options = {
 		host: 'search.twitter.com',
-		path: "/search.json?q=" + requestQuery,
+		path: "/search.json?rpp=100&q=" + requestQuery,
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json'
@@ -63,7 +66,10 @@ function tweetGetter(){
 		console.log("I enter callback");
 		var str = '';
 		response.on('data', function(chunk){
-			str += chunk;
+			if(chunk){
+				str += chunk;
+			}
+			
 			responseJSON += chunk;
 		});
 
@@ -76,6 +82,59 @@ function tweetGetter(){
 	return http.request(options, callback).end();
 
 }
+
+function venueGetter(query, callback2){
+	console.log("current query" + query);
+	var options = {
+		host: "api.foursquare.com",
+		path: "/v2/venues/search?ll=" + query + "&client_id=" + clientId + "&client_secret=" + clientSecret + "&v=20130222",
+		//path: "/v2/venues/search?ll=" + query,
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			'Connection': 'close'
+		}
+	};
+	console.log("path option: " + options.path);
+	callback = function(response){
+		console.log("I enter venue callback");
+		var str = '';
+		response.on('data', function(chunk){
+			if(chunk){
+				str += chunk;
+			}
+			
+			responseJSON += chunk;
+		});
+
+		response.on('end', function(){
+			console.log(str);
+			callback2(str);
+		});
+
+		response.on('err', function(){
+			console.log("We gotta error gettin the venues yo");
+		});
+		//console.log(http.request(options, callback));
+	};
+	console.log("Gonna call the next line");
+	return https.request(options, callback).end();
+}
+
+app.get('/venues/search', function(request, response){
+	var lat = request.query.lat;
+	var lon = request.query.lon;
+	console.log("params = " + request.query);
+	console.log(lat + " " + lon);
+	var venues = venueGetter(lat + ',' + lon, function(resp){
+		return resp;
+	});
+	console.log("***********VENUES************" + venues);
+	response.send({
+		data: JSON.parse(venues),
+		success: (venues !== undefined)
+	});
+});
 
 app.get('/search', function(request, response){
 	console.log("********Le response JSON*******" + responseJSON);
--- static/tw.js
+++ static/tw.js
@@ -1,20 +1,106 @@
 var response;
 
+function Location(lat, lon){
+	this.lat = lat;
+	this.lon = lon;
+	this.setPosition = function(pos){
+		this.lat = pos.lat;
+		this.lon = pos.lon;
+	};
+	this.getPosition = function(){
+		return {
+			"lat": this.lat,
+			"lon": this.lon
+		};
+	};
+}
+
+function getLocation(){
+	var loc = new Location(0,0);
+	if(navigator.geolocation){
+		var timeout = 10 * 1000 * 1000;
+		navigator.geolocation.getCurrentPosition(
+			setLocation,
+			displayError,
+			{
+				enableHighAccuracy: true,
+				timeout: timeout,
+				maximumAge: 0
+			});
+	}
+	else{
+		console.log("Can't do location LAME");
+	}
+
+	function setLocation(position){
+		loc.setPosition({
+			"lat": position.coords.latitude,
+			"lon": position.coords.longitude
+		});
+		console.log("Latitude: " + loc.lat + ", Longitude: " + loc.lon);
+		getVenues(loc);
+
+	}
+
+	function displayError(error) {
+		var errors = {
+			1: 'Permission denied',
+			2: 'Position unavailable',
+			3: 'Request timeout'
+		};
+		alert("Error: " + errors[error.code]);
+	}
+
+	return loc;
+}
 
 function refreshDOM(){
-	console.log(response);
-	$('#tweetcontainer').append('<li>').html(response.results[0].text);
+	var listItem;
+	var paraItem;
+	/*
+	$(response.results.each(function(){
+		var liEl = $("<li>").html(tweet.text);
+		$('')
+	});
+	*/
+	for(var i = 0; i < response.results.length; i++){
+		console.log(response.results[i].text);
+		listItem = $('<li>').html(response.results[i].text);
+		if(response.results[i].geo === null){
+			paraItem = $('<p>').html("null");
+		}
+		else{
+			paraItem = $('<p>').html(response.results[i].geo);
+		}
+		listItem.append(paraItem);
+		$("#tweetcontainer").append(listItem);
+	}
+	
 }
+
 
 function getJSON(){
 	console.log("haha");
+}
+
+function getVenues(loc){
+	$.ajax({
+		type: "get",
+		url: "/venues/search?lat=" + encodeURI(loc.lat) + "&lon=" +  encodeURI(loc.lon),
+		success: function(data){
+			console.log("get success " + data.response);
+			//refreshDOM();
+		}
+	});
 }
 
 function post(q){
 	$.ajax({
 		type: "post",
 		url: "/search.json",
-		data: {"query": q},
+		data: {
+			"query": q
+		},
 		success: function(data){
 			console.log("post success " + data);
 		}
@@ -31,10 +117,11 @@ function get(){
 			//$("#tweetcontainer").append('<li>').html(data);
 			//$("t")
 			console.log(JSON.parse(data.data));
+			refreshDOM();
 		}
 	});
 }
 
 $(document).ready(function(){
-	post("kanye");
+	getLocation();
 });
